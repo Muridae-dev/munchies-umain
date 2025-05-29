30dc60809c2e90b7f18fdad389988d5874f2ccba
Muridae-dev <92300249+Muridae-dev@users.noreply.github.com>
2025-05-29 22:59:51 +0200

    remove redundant element

--- app/components/RestaurantsPage.tsx
+++ app/components/RestaurantsPage.tsx
@@ -39,22 +39,20 @@ export default function RestaurantsPage({
   }, [setRestaurants, setPriceRanges, setRestaurantTypes]);
 
   return (
-    <>
-      <div className="bg-off-white h-screen max-w-screen overflow-hidden pl-[24px] pt-[40px] lg:pl-[40px] lg:pt-[56px] flex flex-col gap-[24px] lg:gap-[48px]">
-        <Link href="/" className="w-fit">
-          <Logo variant="dark" />
-        </Link>
+    <div className="bg-off-white h-screen max-w-screen overflow-hidden pl-[24px] pt-[40px] lg:pl-[40px] lg:pt-[56px] flex flex-col gap-[24px] lg:gap-[48px]">
+      <Link href="/" className="w-fit">
+        <Logo variant="dark" />
+      </Link>
 
-        <div className="flex flex-col lg:flex-row gap-[20px] w-full max-w-full overflow-auto">
-          <div className="shrink-0 pr-[24px] lg:pr-[0px] lg:max-h lg:pb-[24px]">
-            <Filters />
-          </div>
-          <div className="flex flex-col flex-1 overflow-x-hidden">
-            {restaurantTypes[0] && <CategoryTypeList />}
-            <RestaurantList />
-          </div>
+      <div className="flex flex-col lg:flex-row gap-[20px] w-full max-w-full overflow-auto">
+        <div className="shrink-0 pr-[24px] lg:pr-[0px] lg:max-h lg:pb-[24px]">
+          <Filters />
+        </div>
+        <div className="flex flex-col flex-1 overflow-x-hidden">
+          {restaurantTypes[0] && <CategoryTypeList />}
+          <RestaurantList />
         </div>
       </div>
-    </>
+    </div>
   );
 }
